066c8555d93142d68d447758bcc09a3607dfdadf
Shubhradeep Das <118055163+itshubh7@users.noreply.github.com>
2024-05-12 13:07:21 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (79.28%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.cpp
+++ 3110-score-of-a-string/3110-score-of-a-string.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int scoreOfString(string s) {
-        int i, sum=0;
-    for(i=1;i<s.size();i++)
+        int sum=0;
+    for(int i=1;i<s.size();i++)
     {
         sum+=abs(s[i]-s[i-1]);
     }
