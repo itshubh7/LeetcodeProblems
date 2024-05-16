32900afe19f90f5487149ad9168fcc95c88178a5
Shubhradeep Das <118055163+itshubh7@users.noreply.github.com>
2024-05-16 13:21:13 +0530

    Time: 4 ms (32.17%), Space: 11.1 MB (85.58%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -4,9 +4,9 @@ public:
         int n=nums.size();
         if(n==1)
             return 0;
-        if(nums[0]>nums[1])
+        else if(nums[0]>nums[1])
                 return 0;
-        if(nums[n-1]>nums[n-2])
+        else if(nums[n-1]>nums[n-2])
             return n-1;
         else
         {
