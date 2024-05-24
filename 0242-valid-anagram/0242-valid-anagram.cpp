e6e51db711dfacaf7577d878f0ea96be4c10488c
Shubhradeep Das <118055163+itshubh7@users.noreply.github.com>
2024-05-24 19:11:45 +0530

    Time: 9 ms (51.65%), Space: 8.4 MB (82.95%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,13 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()!=t.length())
-            return false;
-        else
-        {
             sort(s.begin(), s.end());
             sort(t.begin(), t.end());
             return (s==t);
-        }
     }
 };
