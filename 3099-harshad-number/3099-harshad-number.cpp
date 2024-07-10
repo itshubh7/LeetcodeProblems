9dd7ef71b94e8f32ffdeac9a3655cd5159a52f1d
Shubhradeep Das <118055163+itshubh7@users.noreply.github.com>
2024-07-10 22:17:50 +0530

    Time: 2 ms (50.09%), Space: 7.2 MB (32.80%) - LeetHub

--- 3099-harshad-number/3099-harshad-number.cpp
+++ 3099-harshad-number/3099-harshad-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int sumOfTheDigitsOfHarshadNumber(int x) {
-        int ret=-1;
+    
         int y=x;
         int sum=0;
         while(x!=0)
@@ -10,7 +10,7 @@ public:
             x/=10;
         }
         if(y%sum==0)
-            ret=sum;
-        return ret;
+           return sum;
+        return -1;
     }
 };
