8ad99b2195b1a57bd478029e645890b5243f4b5d
Shubhradeep Das <118055163+itshubh7@users.noreply.github.com>
2024-08-25 10:18:27 +0530

    Time: 61 ms (44.74%), Space: 51.6 MB (58.94%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,26 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let val = init;
+    return {
+        increment : function() {
+            return ++val;
+        },
+        reset : function() {
+            val = init;
+            return init;
+        },
+        decrement : function() {
+            return --val;
+        }
+    };
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
