69709a523fe4e83cf2bb341fbf0bad380ae86aeb
Shubhradeep Das <118055163+itshubh7@users.noreply.github.com>
2024-10-01 13:27:24 +0530

    Time: 1176 ms (5.16%), Space: 0B (100.00%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select id from weather w1 where temperature > (select temperature from weather w2 where w2.recordDate  = DATE_SUB(w1.recordDate, INTERVAL 1 DAY))
